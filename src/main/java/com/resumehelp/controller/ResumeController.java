ca02f735bf8e61d6161823ea1f38001957e5971d
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-03-13 15:27:23 +0530

    comu

--- src/main/java/com/resumehelp/controller/ResumeController.java
+++ src/main/java/com/resumehelp/controller/ResumeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api") // ✅ Base path for all API
-@CrossOrigin(origins = "http://localhost:3000") // ✅ Frontend CORS allowed
+@CrossOrigin(origins = "https://ai-resume-frontend-mg.vercel.app/") // ✅ Frontend CORS allowed
 public class ResumeController {
 
     @Autowired
