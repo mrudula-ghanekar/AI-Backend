8dcd5b8823ed3252488b608f045ee0a66d3c0200
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-03-26 11:19:59 +0530

    Update ResumeController.java

--- src/main/java/com/resumehelp/controller/ResumeController.java
+++ src/main/java/com/resumehelp/controller/ResumeController.java
@@ -1,3 +1,16 @@
+package com.resumehelp.controller;
+
+import com.resumehelp.service.OpenAIService;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class ResumeController {
