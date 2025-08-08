ee9110144ad10b94dc0cdfbff58f470e03fb49bd
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-08-09 00:50:37 +0530

    Update CorsConfig.java

--- src/main/java/com/resumehelp/config/CorsConfig.java
+++ src/main/java/com/resumehelp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // Allow all API routes
-                        .allowedOrigins("https://resume-help-test-frontend.vercel.app") //prod
+                        .allowedOrigins("https://ai-resume-frontend-mg.vercel.app/") //prod
                         .allowedMethods("GET", "POST", "OPTIONS") // Allow POST & OPTIONS
                         .allowedHeaders("*") // All headers
                         .allowCredentials(true); // Allow cookies if needed
